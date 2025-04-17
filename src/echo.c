2fcae0a9e0707917069f9ac694c4ea090a7f2571
Chiara Kappe <chiarakappe@Chiaras-MacBook-Pro-2.local>
2025-04-18 01:28:02 +0200

    started sketching out echo

--- src/echo.c
+++ src/echo.c
@@ -3,16 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ckappe <ckappe@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: chiarakappe <chiarakappe@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 14:24:35 by ckappe            #+#    #+#             */
-/*   Updated: 2025/04/15 15:00:16 by ckappe           ###   ########.fr       */
+/*   Updated: 2025/04/18 01:25:18 by chiarakappe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../minishell.h"
+#include "/../minishell.h"
 
-// int	echo_builtin(t_token **token)
-// {
+
+/* 
+echo hallo       -n
+hallo -n */
+
+int	echo_builtin(t_token **token)
+{
+	t_token *cur;
+	int		newline;
+	int		first_arg;
+
+	cur = token;
+	newline = 1;
+	first_arg = 1;
+	// skip echo command
+	while (cur && cur->type != ARGS)
+		cur = cur->next;
+	// check for -n flag
+	if (cur && cur->type == ARGS && !ft_strcmp(cur->value, "-n"))
+	{
+		newline = 0;
+		cur = cur->next;
+	}
+	// print args
+	while (cur && )
 	
-// }
+	if (newline)
+		printf("\n");
+	return (EXIT_SUCCESS);	
+}
+
